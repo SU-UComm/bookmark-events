c3b9b3e43412e78fe9303a946d930c5d47ec83ab
Justin Rent <>
2022-04-21 13:04:21 -0400

    - Updates to index.php to import new head, header, and footer partials - Adjust classes in index.php for Localist styles - Adds /includes directory with new head, header, and footer partials

--- index.php
+++ index.php
@@ -25,81 +25,81 @@ if ( !empty( $_POST ) ) {
 <html lang="en">
 <!--<![endif]-->
 
-<head>
-  <title>Bookmark | Stanford Event Calendar</title>
+  <head>
+    <title>Bookmark | Stanford Event Calendar</title>
 
-  <!-- Meta -->
-  <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="author" content="Stanford Event Calendar" />
-  <meta name="description" content="Bookmark Localist events." />
+    <!-- Meta -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="author" content="Stanford Event Calendar" />
+    <meta name="description" content="Bookmark Localist events." />
 
-  <?php include '../v5/includes/head.html'; ?>
-  <style>
-    input[type="radio"] {
-      margin-bottom: 1em;
-      margin-right:  0.5em;
-    }
-  </style>
-</head>
+    <?php include './includes/head.html'; ?>
+    <style>
+      input[type="radio"] {
+        margin-bottom: 1em;
+        margin-right:  0.5em;
+      }
+    </style>
+  </head>
 
-<body class="content-page">
-<?php include '../v5/includes/header.html'; ?>
+  <body class="su-content-page su-bookmark-page">
+  <?php include './includes/header.html'; ?>
 
-<!-- main content -->
-<section id="main-content"  role="main">
-  <div class="container">
-    <h1>Bookmark a Localist event</h1>
+    <!-- main content -->
+    <main id="main-content"  role="main">
+      <div class="container">
+        <h1>Bookmark a Localist event</h1>
 
-<?php if ( !empty( $_POST ) ) { ?>
-    <h2>Add to feed</h2>
-    <?php if ( $num_feeds == 0 ) { ?>
-      <p>
-        You are not yet registered to bookmark events in any feeds. Please
-        <a href="http://stanford.io/contact-events-calendar">submit a ticket</a>
-        to request access to bookmarking. Please indicate in your ticket what
-        feed(s) you'd like to add events to.
-      </p>
+    <?php if ( !empty( $_POST ) ) { ?>
+        <h2>Add to feed</h2>
+        <?php if ( $num_feeds == 0 ) { ?>
+          <p>
+            You are not yet registered to bookmark events in any feeds. Please
+            <a href="http://stanford.io/contact-events-calendar">submit a ticket</a>
+            to request access to bookmarking. Please indicate in your ticket what
+            feed(s) you'd like to add events to.
+          </p>
+        <?php } else { ?>
+        <p>
+          Add <a href="<?php echo $event->localist_url; ?>"><?php echo $event->title; ?></a> to:
+        </p>
+        <form id="bookmark-form" method="post" action="<?php echo $root; ?>/bookmark.php">
+            <input name="eventId" type="hidden" value="<?php echo $_POST[ 'eventId' ]; ?>">
+          <?php if ( $num_feeds <= 5 ) { ?>
+            <?php $checked = ( $num_feeds == 1 ) ? 'checked="checked"' : ''; ?>
+            <?php foreach ( $feeds as $fid => $theFeed ) { ?>
+              <input name="feedId" type="radio" value="<?php echo $fid; ?>" <?php echo $checked; ?> />&nbsp;
+              <a href="<?php echo $root; ?>/feed.php?slug=<?php echo $theFeed->slug; ?>">
+                <?php echo $theFeed->name, ' (', $theFeed->slug, ')'; ?>
+              </a></br>
+            <?php } ?>
+          <?php } else { ?>
+          <select name="feedId">
+            <?php foreach ( $feeds as $fid => $theFeed ) { ?>
+              <option value="<?php echo $fid; ?>"/>
+                <?php echo $theFeed->name, ' (', $theFeed->slug, ')'; ?>
+              </option>
+            <?php } ?>
+          </select>
+          <?php } ?>
+          <input type="submit" value="Add to selected feed">
+        </form>
+        <?php } ?>
     <?php } else { ?>
-    <p>
-      Add <a href="<?php echo $event->localist_url; ?>"><?php echo $event->title; ?></a> to:
-    </p>
-    <form id="bookmark-form" method="post" action="<?php echo $root; ?>/bookmark.php">
-        <input name="eventId" type="hidden" value="<?php echo $_POST[ 'eventId' ]; ?>">
-      <?php if ( $num_feeds <= 5 ) { ?>
-        <?php $checked = ( $num_feeds == 1 ) ? 'checked="checked"' : ''; ?>
-        <?php foreach ( $feeds as $fid => $theFeed ) { ?>
-          <input name="feedId" type="radio" value="<?php echo $fid; ?>" <?php echo $checked; ?> />&nbsp;
-          <a href="<?php echo $root; ?>/feed.php?slug=<?php echo $theFeed->slug; ?>">
-            <?php echo $theFeed->name, ' (', $theFeed->slug, ')'; ?>
-          </a></br>
-        <?php } ?>
-      <?php } else { ?>
-      <select name="feedId">
-        <?php foreach ( $feeds as $fid => $theFeed ) { ?>
-          <option value="<?php echo $fid; ?>"/>
-            <?php echo $theFeed->name, ' (', $theFeed->slug, ')'; ?>
-          </option>
-        <?php } ?>
-      </select>
-      <?php } ?>
-      <input type="submit" value="Add to selected feed">
-    </form>
+        <form id="test-form" method="post">
+          <label for="userId">User id:</label>
+          <input name="userId" type="text" width="15" /><br/>
+          <label for="eventId">Event id:</label>
+          <input name="eventId" type="text" width="15" /><br/>
+          <input type="submit" value="Bookmark this event">
+        </form>
     <?php } ?>
-<?php } else { ?>
-    <form id="test-form" method="post">
-      <label for="userId">User id:</label>
-      <input name="userId" type="text" width="15" /><br/>
-      <label for="eventId">Event id:</label>
-      <input name="eventId" type="text" width="15" /><br/>
-      <input type="submit" value="Bookmark this event">
-    </form>
-<?php } ?>
-  </div>
-</section>
+      </div>
+    </main>
 
-<!-- BEGIN footer -->
-<?php include '../v5/includes/footer.html'; ?>
-<!-- END footer -->
-</body>
+    <!-- BEGIN footer -->
+    <?php include './includes/footer.html'; ?>
+    <!-- END footer -->
+  </body>
 </html>
